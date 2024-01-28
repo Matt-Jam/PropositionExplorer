392f8dc94f3c9afdcddcfd2939f666dd9d1a43d2
Matt-Jam <mattyjamson@gmail.com>
2024-01-28 23:01:16 +0000

    Refactored yesod

--- helloworld.hs
+++ helloworld.hs
@@ -29,10 +29,9 @@ data PropInput = PropInput
     }
     deriving Show
 
-getHomeR :: Handler Html
-getHomeR = defaultLayout 
-    [whamlet|
-            <form action=@{InputR}>
+base :: Widget
+base = [whamlet|
+<form action=@{InputR}>
                 <h3>
                     Instructions 
                 <p>
@@ -64,46 +63,19 @@ getHomeR = defaultLayout
                     <input type=text name=str>
                     <input type=submit value="Process">
     |]
+    
+getHomeR :: Handler Html
+getHomeR = defaultLayout base
+
 
 getInputR :: Handler Html
 getInputR = do
     prop <- runInputGet $ PropInput
                 <$> ireq textField "str"
     defaultLayout $ do
+        base 
         let p = processTextExpression (unpack $ str prop)
         [whamlet|
-            <form action=@{InputR}>
-                <h3>
-                    Instructions 
-                <p>
-                    Input a valid boolean expression using the following key
-                <table>
-                    <tr>
-                        <td>True
-                        <td>T
-                    <tr>
-                        <td>False
-                        <td>F
-                    <tr>
-                        <td> Variable 
-                        <td> A character
-                    <tr>
-                        <td> And
-                        <td>*
-                    <tr>
-                        <td> Or
-                        <td> +
-                    <tr>
-                        <td> Not
-                        <td> -
-                <p>
-                    Example: (a+b)*(-c)
-                <h3>
-                    Input:
-                <p>
-                    <input type=text name=str>
-                    <input type=submit value="Process">
-                <h3>
                     Simplified expression:
                 <p>
                     #{show p }
