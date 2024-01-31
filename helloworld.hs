92567f2fb1de69a783d8c31e1301352218858249
Matt-Jam <mattyjamson@gmail.com>
2024-01-31 23:18:19 +0000

    https

--- helloworld.hs
+++ helloworld.hs
@@ -19,7 +19,7 @@ mkYesod "App" [parseRoutes|
 |]
 
 instance Yesod App where 
-    approot = ApprootStatic "http://mattjam.me"
+    approot = ApprootStatic "https://mattjam.me"
 
 instance RenderMessage App FormMessage where
     renderMessage _ _ = defaultFormMessage
